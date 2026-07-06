fa3b748b154ea65e3abf00e2fed9e4cad004cf3b
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-07-06 15:27:55 +0200

    add: using useState

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,21 +1,23 @@
+import {useState} from 'react';
+
 import './TaskList.css';
 import TaskItem from './TaskItem.tsx';
 
 const title = 'My Todo App';
-
 const items = [
   { id: 1, text: 'Task 1', completed: false },
   { id: 2, text: 'Task 2', completed: true },
   { id: 3, text: 'Task 3', completed: false },
 ];
-
-function clickHandler() {
-    alert('Task clicked!');
-}
-
-
-
+// Ausprobieren: text conditional evaluating in einer Funktion, anstatt direkt mit z.B.: einem ternären Operator in JSX zu evaluieren. Das ist eine gute Übung, um die Logik von JSX zu trennen und den Code sauberer zu gestalten.
 export default function TaskList() {
+    const [text, setText] = useState('');
+
+    function clickHandler() {
+        alert('Task clicked!');
+        setText('Task clicked!');
+    }
+
     return(
         <div id="task-list">
             <div>
@@ -28,6 +30,7 @@ export default function TaskList() {
                     ))}
                 </ul>
             </div>
+            {text ? <div>{text}</div> : ''}
         </div>
         );
     }
