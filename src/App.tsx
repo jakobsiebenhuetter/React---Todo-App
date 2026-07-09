e12bcc08f0e74d31110235440608bbaa44eca6d4
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-07-09 15:04:37 +0200

    add: Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Header from './components/Header.tsx';
-import TaskList from './components/TaskList.tsx';
+import Header from "./components/Header.tsx";
+import TaskList from "./components/TaskList.tsx";
 
 export default function App() {
   return (
-    <div>
+    <>
       <Header />
       <TaskList />
-    </div>
+    </>
   );
 }
