670ca598cf1b36199d1257cec2109ab929c67415
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-07-10 19:55:23 +0200

    add: Tasks hinzufügen, styling

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -5,14 +5,15 @@ import TaskItem from './TaskItem.tsx';
 
 const title = 'My Todo App';
 const items = [
-  { id: 1, text: 'Task 1', completed: false },
-  { id: 2, text: 'Task 2', completed: false },
-  { id: 3, text: 'Task 3', completed: false },
+  { id: 1, text: 'Task 1', completed: false, createdat: new Date() },
+  { id: 2, text: 'Task 2', completed: false, createdat: new Date() },
+  { id: 3, text: 'Task 3', completed: false, createdat: new Date() },
 ];
 // Ausprobieren: text conditional evaluating in einer Funktion, anstatt direkt mit z.B.: einem ternären Operator in JSX zu evaluieren. Das ist eine gute Übung, um die Logik von JSX zu trennen und den Code sauberer zu gestalten.
 export default function TaskList() {
     const [selectId, setSelectId] = useState(0);
     const [taskItems, setTaskItems] = useState(items);
+    const [value, setValue] = useState('');
 
     console.log('selectId: ', selectId);
     function clickHandler(taskId: number) {
@@ -22,7 +23,25 @@ export default function TaskList() {
                 return item.id === taskId ? {...item, completed: !item.completed} : item;
             })
         })
+    }
 
+    function setTask(event) {
+        setValue(event.target.value);
+    }
+
+    function addTask() {
+        if(value.trim() === '') 
+            return;
+
+        const newTask = {
+            id: taskItems.length + 1,
+            text: value,
+            completed: false,
+            createdat: new Date()
+        }
+
+        setTaskItems((prevTaskItems) => [...prevTaskItems, newTask]);
+        setValue('');
     }
 
 
@@ -33,9 +52,13 @@ export default function TaskList() {
             </div>
             <div>
                 <h2>Aufgabenliste</h2>
+            <div id="add-task">
+                <input type="text" placeholder="Neue Aufgabe hinzufügen..." value={value} onChange={setTask}/>
+                <button onClick={addTask}>+</button>
+            </div>
                 <ul>
                     {taskItems.map((item) => 
-                     <TaskItem 
+                     <TaskItem key={item.id}
                      isSelected={item.completed}
                      onClick={() => clickHandler(item.id)}
                      >{item.text}
