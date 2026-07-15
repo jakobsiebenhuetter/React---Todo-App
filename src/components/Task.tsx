c8f0c1f90fdbcaf1e77ef72abb38ce69470d03ef
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-07-15 17:37:31 +0200

    change: Task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,18 +1,18 @@
-import "./TaskItem.css";
+import "./Task.css";
 
 import TaskItem from "./TaskItem.tsx";
 import UpdateTaskItem from "./UpdateTask.tsx";
 
 export default function Task({ deleteTask, onUpdate, update, onUpdateTask, completeTask,  ...props}) {
 
-  let item = <TaskItem deleteTask={deleteTask} onUpdateTask={onUpdateTask} completeTask={completeTask}>{props.children}</TaskItem>
+  let item = <TaskItem deleteTask={deleteTask} onUpdateTask={onUpdateTask} completed={props.completed} completeTask={completeTask}>{props.children}</TaskItem>
 
   if (onUpdate) {
     item = <UpdateTaskItem updateTaskText={update} haveId={props.haveId} text={props.text}></UpdateTaskItem>
   }
 
   return (
-    <li id="delete-task-btn" {...props}>
+    <li id="task">
       {item}
     </li>
   );
