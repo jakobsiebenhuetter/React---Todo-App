db73065e72ddcf414ea943e89b59f573fd946f63
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-07-15 17:37:12 +0200

    change: global styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,6 @@
 import './Header.css';
 
 
-const colors = ['#FF5733', '#33FF57', '#3357FF', '#F333FF', '#33FFF5'];
-colors.map(() => randomColor());
-function randomColor() {
-    //...
-}
-
-
-
-
 
 export default function Header() {
   return (
@@ -18,7 +9,6 @@ export default function Header() {
             <span>Icon</span>
             <div style={{marginLeft: '10px'}}>Flowlist</div>
         </div>
-         <h1>Hallo Welt, ich wurde mit React erstellt :-)!</h1>
         <ul style={{ display: 'flex', listStyleType: 'none', marginRight: '10px' }}>
             <li style={{ marginRight: '10px' }}>
                 <a href="#">Anmelden</a>
