2e1f6f944f7ebf2cbcb98725caa382f9e5ebfc8a
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-07-21 21:33:58 +0200

    add: component composition

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,102 @@
+import AddTask from "./components/AddTask.tsx";
 import Header from "./components/Header.tsx";
 import TaskList from "./components/TaskList.tsx";
+import Task from "./components/Task.tsx";
+import {useState} from 'react';
 
+export type TTask = {
+  id: number;
+  text: string;
+  completed: boolean;
+  createdat: Date;
+  updating: boolean;
+}
 // const items = [
 //   { id: Math.random(), text: 'Task 1', completed: false, createdat: new Date(), updating: false},
 //   { id: Math.random(), text: 'Task 2', completed: false, createdat: new Date() , updating: false},
 //   { id: Math.random(), text: 'Task 3', completed: false, createdat: new Date() , updating: false},
 // ];
 
-// Next Step mit Komposition Komponents arbeiten
+// Next Step mit Components Composition arbeiten
 const tasks = localStorage.getItem('tasks');
-let parsedTasks = [];
+let parsedTasks: TTask[] = [];
 if(tasks) {
   parsedTasks = JSON.parse(tasks);
 }
 
 export default function App() {
+
+  const [tasks, setTasks] = useState(parsedTasks);
+  
+  function addTask(newTask) {
+    setTasks((prevTaskItems) => {
+      const allTasks = [...prevTaskItems, newTask];
+      const stringTasks = JSON.stringify(allTasks);
+      localStorage.setItem('tasks', stringTasks);
+      return [...allTasks];
+    });
+  }
+  
+  function deleteTask(taskId: number) {
+    setTasks((prevTaskItems) => {
+      const allTasks = prevTaskItems.filter((task) => task.id !== taskId)
+      const stringTasks = JSON.stringify(allTasks);
+      localStorage.setItem('tasks', stringTasks);
+      
+      return allTasks;
+    });
+  }
+  
+  function updateTask(taskId: number) {
+    setTasks((prevTaskItems) => {
+      const allTasks = prevTaskItems.map((task) => {
+        return task.id === taskId ? {...task, updating: true} : task;
+      });
+      
+      const stringTasks = JSON.stringify(allTasks);
+      localStorage.setItem('tasks', stringTasks);
+      
+      return allTasks;
+    });
+  }
+  
+  function updateTaskText(taskId: number, newText: string) {
+    setTasks((prevTaskItems) => {
+      const allTasks = prevTaskItems.map((item) => {
+        return item.id === taskId ? {...item, text: newText, updating: false} : item;
+      });
+      
+      const stringTasks = JSON.stringify(allTasks);
+      localStorage.setItem('tasks', stringTasks);
+      
+      return allTasks;
+    });
+  }
+  
+  function completeTask(taskId: number) {
+    setTasks((prevTaskItems) => {
+      const allTasks =  prevTaskItems.map((task) => {
+        return task.id === taskId ? {...task, completed: !task.completed} : task;
+      });
+      
+      const stringTasks = JSON.stringify([...allTasks]);
+      localStorage.setItem('tasks', stringTasks);
+      
+      return allTasks;
+    });
+  }
+  
   return (
     <>
       <Header />
-      <TaskList tasks={parsedTasks}/>
+      <AddTask addTask={addTask}/>
+      <TaskList>
+        {tasks.length > 0 ? tasks.map((item) => 
+          <Task key={item.id} taskProps={item} onUpdateTask={() => updateTask(item.id)} update={updateTaskText} deleteTask={() => {deleteTask(item.id)}} completeTask={() => completeTask(item.id)}>
+            {item.text}
+          </Task>) 
+          : <li>Keine Aufgaben vorhanden</li>}
+      </TaskList>
     </>
   );
 }
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,67 +1,10 @@
-import {useState} from 'react';
-
 import './TaskList.css';
-import Task from './Task.tsx';
-import AddTask from './AddTask.tsx';
 
 const title = 'My Todo App';
 
 // Mit richtigen ids arbeiten
-export default function TaskList({tasks}) {
-    const [taskItems, setTaskItems] = useState(tasks);
-
-    function addTask(newTask) {
-        setTaskItems((prevTaskItems) => {
-            const allTasks = [...prevTaskItems, newTask];
-            const stringTasks = JSON.stringify(allTasks);
-            localStorage.setItem('tasks', stringTasks);
-            return [...allTasks];
-        });
-    }
-
-    function deleteTask(taskId: number) {
-        setTaskItems((prevTaskItems) => {
-            const allTasks = prevTaskItems.filter((task) => task.id !== taskId)
-            const stringTasks = JSON.stringify(allTasks);
-            localStorage.setItem('tasks', stringTasks);
-            return allTasks;
-        });
-    }
-
-    function updateTask(taskId: number) {
-        setTaskItems((prevTaskItems) => {
-            const allTasks = prevTaskItems.map((task) => {
-                return task.id === taskId ? {...task, updating: true} : task;
-            });
-            const stringTasks = JSON.stringify(allTasks);
-            localStorage.setItem('tasks', stringTasks);
-            return allTasks;
-        });
-    }
-
-    function updateTaskText(taskId: number, newText: string) {
-        setTaskItems((prevTaskItems) => {
-            const allTasks = prevTaskItems.map((item) => {
-                return item.id === taskId ? {...item, text: newText, updating: false} : item;
-            });
-            const stringTasks = JSON.stringify(allTasks);
-            localStorage.setItem('tasks', stringTasks);
-            return allTasks;
-        });
-    }
-
-
-    function completeTask(taskId: number) {
-        setTaskItems((prevTaskItems) => {
-            const allTasks =  prevTaskItems.map((task) => {
-                return task.id === taskId ? {...task, completed: !task.completed} : task;
-            })
-            const stringTasks = JSON.stringify([...allTasks]);
-            localStorage.setItem('tasks', stringTasks);
-            return allTasks;
-        })
-    }
-
+export default function TaskList({children}) {
+ 
     return(
         <div id="task-list">
             <div>
@@ -69,20 +12,8 @@ export default function TaskList({tasks}) {
             </div>
             <div>
                 <h2>Aufgabenliste</h2>
-                <AddTask addTask={addTask}></AddTask>
                 <ul>
-                    {taskItems.length > 0 ? 
-                    taskItems.map((item) => 
-                     <Task
-                     key={item.id}
-                     taskProps={item}
-                     onUpdateTask={() => updateTask(item.id)}
-                     update={updateTaskText}
-                     deleteTask={() => {deleteTask(item.id)}}
-                     completeTask={() => completeTask(item.id)}
-                     >{item.text}
-                    </Task>) 
-                    : <li>Keine Aufgaben vorhanden</li>}
+                    {children}
                 </ul>
             </div>
         </div>
