8b5b73759b8a9b826fe234947e7e3a986de9f07a
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-08-14 09:04:34 +0200

    change: positioning Contextmenu

--- src/components/Contextmenu.tsx
+++ src/components/Contextmenu.tsx
@@ -1,5 +1,6 @@
 import './Contextmenu.css';
 import { createPortal } from 'react-dom';
+// import { useState, useEffect } from 'react';
 
 export type TDropDownItem = {
     id: string,
@@ -14,10 +15,24 @@ interface IDropDownProps {
     posY: number
 }
 
+
+
 export default function DropDown({items, posX, posY}: IDropDownProps) {
+    // const [isMobile, setIsMobile] = useState(false);
+    
+    // useEffect(() => {
+    //     const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+    //     const handler = (e: MediaQueryListEvent) => setIsMobile(e.matches);
+        
+    //     mediaQuery.addEventListener('change', handler)
+        
+    //     return () => mediaQuery.removeEventListener('change', handler)
+    // }, []);
+
     return createPortal(
         <>
-        <div className="" style={{top: `${posY}px`, left: `${posX + 15}px`}}>
+        <div className={'dropdown'} style={{top: `${posY}px`, left: `${posX + 15}px`} }>
             <ul>
                 {items.length && items.map(item => (
                     <li key={item.id} id={item.id} className='dropdown-item' onClick={item.onClick}>
