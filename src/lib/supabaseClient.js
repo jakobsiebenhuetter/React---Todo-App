354961eedfee6563a4409746163d84270d2e6bb7
Jakob Siebenhütter <jakob.siebenhuetter@gmx.at>
2026-08-30 12:28:42 +0200

    change

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
-export const supabase = createClient(supabaseUrl, supabasePublishableKey)
+export const supabase = createClient('https://tdnsixpaxjkqjnzwghbf.supabase.co', 'sb_publishable_xCTg3XHP-YDR9oqCri7NdQ_nlByLPlb')
